caf7bad301763bce4815cce1094c52352a9c5833
Victor-Iulian Sandu <vyctor661@gmail.com>
2024-11-16 15:53:06 +0200

    fix csv file inclusion and added assembly info file

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// General Information
+[assembly: AssemblyTitle("Color Names")]
+[assembly: AssemblyDescription("A discord bot with generic functionalities, also sentient.")]
+
+// Version information
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
